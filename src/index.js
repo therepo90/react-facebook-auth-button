d8c0887c4f4724d5f3c8187c6989214866ba0c5e
Tomasz Sz <t.szepczynski@gmail.com>
2018-06-30 11:34:36 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class FacebookAuth extends React.Component {
+  render() {
+    return (
+      <div>This is facebook auth!</div>
+    );
+  }
+}
+export default FacebookAuth;
